eccfd6c35962cb9f6a57eec6ebf588435e49f7b8
ponvannan.p <rajaponvan666@gmail.com>
2025-10-06 17:19:32 +0530

    XML Commit

--- src/test/java/com/thetestingacademy/ex_06_PayloadManagement/Classes/AI/BookingDates.java
+++ src/test/java/com/thetestingacademy/ex_06_PayloadManagement/Classes/AI/BookingDates.java
@@ -1,25 +1,25 @@
-package com.thetestingacademy.ex_06_PayloadManagement.Classes.AI;
-
-public class BookingDates {
-
-        private String checkin;
-        private String checkout;
-
-        // Getters and Setters
-        public String getcheckin() {
-            return checkin;
-        }
-
-        public void setcheckin(String checkin) {
-            this.checkin = checkin;
-        }
-
-        public String getcheckout() {
-            return checkout;
-        }
-
-        public void setcheckout(String checkout) {
-            this.checkout = checkout;
-        }
-    }
-
+//package com.thetestingacademy.ex_06_PayloadManagement.Classes.AI;
+//
+//public class BookingDates {
+//
+//        private String checkin;
+//        private String checkout;
+//
+//        // Getters and Setters
+//        public String getcheckin() {
+//            return checkin;
+//        }
+//
+//        public void setcheckin(String checkin) {
+//            this.checkin = checkin;
+//        }
+//
+//        public String getcheckout() {
+//            return checkout;
+//        }
+//
+//        public void setcheckout(String checkout) {
+//            this.checkout = checkout;
+//        }
+//    }
+//
--- src/test/java/com/thetestingacademy/ex_06_PayloadManagement/Classes/AI/booking.java
+++ src/test/java/com/thetestingacademy/ex_06_PayloadManagement/Classes/AI/booking.java
@@ -1,60 +1,73 @@
-package com.thetestingacademy.ex_06_PayloadManagement.Classes.AI;
-
-public class booking {
-    private String firstname;
-    private String lastname;
-    private int totalprice;
-    private boolean depositpaid;
-    private BookingDates bookingdates;
-    private String additionalneeds;
-
-    // Getters and Setters
-    public String getfirstname() {
-        return firstname;
-    }
-
-    public void setfirstname(String firstname) {
-        this.firstname = firstname;
-    }
-
-    public String getlastname() {
-        return lastname;
-    }
-
-    public void setlastname(String lastname) {
-        this.lastname = lastname;
-    }
-
-    public int gettotalprice() {
-        return totalprice;
-    }
-
-    public void settotalprice(int totalprice) {
-        this.totalprice = totalprice;
-    }
-
-    public boolean isdepositpaid() {
-        return depositpaid;
-    }
-
-    public void setdepositpaid(boolean depositpaid) {
-        this.depositpaid = depositpaid;
-    }
-
-    public BookingDates getbookingdates() {
-        return bookingdates;
-    }
-
-    public void setbookingdates(BookingDates bookingdates) {
-        this.bookingdates = bookingdates;
-    }
-
-    public String getadditionalneeds() {
-        return additionalneeds;
-    }
-
-    public void setadditionalneeds(String additionalneeds) {
-        this.additionalneeds = additionalneeds;
-    }
-}
-
+//package com.thetestingacademy.ex_06_PayloadManagement.Classes.AI;
+//
+//import java.util.LinkedHashMap;
+//import java.util.Map;
+//
+//public class booking {
+//
+//    private String firstname;
+//    private String lastname;
+//    private Integer totalprice;
+//    private Boolean depositpaid;
+//    private BookingDates bookingdates;
+//    private String additionalneeds;
+//    private Map<String, Object> additionalProperties = new LinkedHashMap<>();
+//
+//    // Getters and Setters
+//    public String getFirstname() {
+//        return firstname;
+//    }
+//
+//    public void setFirstname(String firstname) {
+//        this.firstname = firstname;
+//    }
+//
+//    public String getLastname() {
+//        return lastname;
+//    }
+//
+//    public void setLastname(String lastname) {
+//        this.lastname = lastname;
+//    }
+//
+//    public Integer getTotalprice() {
+//        return totalprice;
+//    }
+//
+//    public void setTotalprice(Integer totalprice) {
+//        this.totalprice = totalprice;
+//    }
+//
+//    public Boolean getDepositpaid() {
+//        return depositpaid;
+//    }
+//
+//    public void setDepositpaid(Boolean depositpaid) {
+//        this.depositpaid = depositpaid;
+//    }
+//
+//    public BookingDates getBookingdates() {
+//        return bookingdates;
+//    }
+//
+//    public void setBookingdates(BookingDates bookingdates) {
+//        this.bookingdates = bookingdates;
+//    }
+//
+//    public String getAdditionalneeds() {
+//        return additionalneeds;
+//    }
+//
+//    public void setAdditionalneeds(String additionalneeds) {
+//        this.additionalneeds = additionalneeds;
+//    }
+//
+//    public Map<String, Object> getAdditionalProperties() {
+//        return additionalProperties;
+//    }
+//
+//    public void setAdditionalProperty(String name, Object value) {
+//        this.additionalProperties.put(name, value);
+//    }
+//}
+//
